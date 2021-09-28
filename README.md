868b733d5d9d42685585fdc9d1aea1e5dd4ca3fa
TechGeekNeXt <techgeeknext@gmail.com>
2021-09-28 18:35:38 +0530

    techgeeknext

---
+++ README.md
@@ -0,0 +1,23 @@
+### TechGeekNext > https://www.techgeeknext.com/
+
+# Spring Boot Swagger 3 Security Example | TechGeekNxt >> 
+
+> [Spring Boot Swagger 3 Security Example](https://www.techgeeknext.com/spring-boot/spring-boot-swagger3-security-example/)
+
+## Run Spring Boot application
+```
+mvn spring-boot:run
+```
+## Test Spring Boot JPA CRUD Example
+<ol>
+<li>Open http://localhost:8080/swagger-ui.html in browser to view the Swagger UI documentation. </li>
+<li>You can notice the "open lock icon", because we have not done Authorization yet.</li>
+<li>Click on <b>Authorize</b> button and provide username (techgeeknext) and password (techgeeknext).</li>
+<li>Once Authorization is done, we can see the "close lock icon".</li>
+<li>And now we can access any rest end point to get the response.</li>
+</ol>
+
+### Previous tutorial
+> [Spring Boot Swagger 2 Security Example](https://www.techgeeknext.com/spring-boot/spring-boot-swagger2-example/)
+
+> [Spring Boot Swagger 3 Security Example](https://www.techgeeknext.com/spring-boot/spring-boot-swagger3-example/)
---
+++ src/main/java/com/techgeeknext/configuration/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.techgeeknext.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/swagger-ui*/**", "/techgeeknext-openapi/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("techgeeknext")
+                .password(passwordEncoder().encode("techgeeknext"))
+                .authorities("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
